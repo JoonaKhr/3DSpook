a4cb3b8a0b1884f094ece13eaa1582c1075fe2ef
Joona Kähärä <joonakhr@gmail.com>
2023-10-05 15:59:10 +0300

    raycasting left click and wasd movement

--- PlayerController.gd
+++ PlayerController.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const RAY_LENGTH = 1000
 var rot_x = 0
 var rot_y = 0
 
@@ -22,8 +23,19 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
+	var space_state = get_world_3d().direct_space_state
 	var cam = $Pivot/Camera3D
 	var mousepos = get_viewport().get_mouse_position()
+
+	var origin = cam.project_ray_origin(mousepos)
+	var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+
+	var result = space_state.intersect_ray(query)
+	if Input.is_mouse_button_pressed(1):
+		if result:
+			print(result)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -34,8 +46,8 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("left", "right", "forwards", "backwards")
+	var direction = ($Pivot.transform.basis.rotated(Vector3.UP, rotation.y) * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
