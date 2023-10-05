d0133a6fcd8697c83c799bddd40d5c08f745c9c2
Zuul-Fool <48661077+Zuul-Fool@users.noreply.github.com>
2023-10-05 16:22:42 +0300

    Stuff done

---
+++ Scripts/menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@export_file("*tscn") var level
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ResourceLoader.load_threaded_request(level)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_quit_pressed():
+	pass
+
+func _on_start_pressed():
+	get_tree().quit()
