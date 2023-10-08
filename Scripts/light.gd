788b6c4b90940b833125aaf9b12ba60287edba27
Joona Kähärä <joonakhr@gmail.com>
2023-10-08 14:58:44 +0300

    lamps have semi-random flickering

--- Scripts/light.gd
+++ Scripts/light.gd
@@ -1,17 +1,22 @@
 extends Node3D
 
 @onready var flickoff = $FlickoffTimer
-@onready var flickon = $FlickonTimer
 @onready var light = $OmniLight3D
+@onready var animations = $AnimationPlayer
 
 func _ready():
-	flickoff.wait_time = randf_range(240, 480)
+	flickoff.wait_time = randf_range(120, 240)
 	
 
 
 func interact():
-	print("wow such interaction")
 	if light.visible:
+		flickoff.stop()
 		light.visible = false
 	else:
+		flickoff.start()
 		light.visible = true
+
+
+func _on_flickoff_timer_timeout():
+	$AnimationPlayer.play(animations.get_animation_list()[randi_range(0, animations.get_animation_list().size()-1)])
