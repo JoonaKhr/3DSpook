39978cf08a30316e5534545c06b30d317ac50455
Joona Kähärä <joonakhr@gmail.com>
2023-10-15 15:06:48 +0300

    Normal first person movement poggies

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,26 +1,28 @@
 extends CharacterBody3D
 
-
 @export var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const RAY_LENGTH = 5
 var rot_x = 0
 var rot_y = 0
-@export var inventory: PackedScene
+var inventory
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	inventory = $inventory
 
 func _input(event):
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			inventory.changeHeldItem(event.button_index)
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			inventory.changeHeldItem(event.button_index)
+				
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * 0.002)
 		$Pivot.rotate_x(-event.relative.y * 0.002)
 		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
-
-
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -54,13 +56,14 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var input_dir = Input.get_vector("left", "right", "forwards", "backwards")
-	var direction = ($Pivot.transform.basis.rotated(Vector3.UP, rotation.y) * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	print(velocity)
 	move_and_slide()
--- Scripts/item.gd
+++ Scripts/item.gd
@@ -3,3 +3,5 @@ extends Node3D
 var n = ""
 @export var itemIcon: Image
 
+func pickupItem(pickupee):
+    pickupee.inventory.obtainItem(self)
