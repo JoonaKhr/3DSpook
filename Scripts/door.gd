8dfac37d6cd4ca5b874ab0c1d7577b4a1bbf4931
Joona Kähärä <joonakhr@gmail.com>
2023-10-22 20:26:08 +0300

    better door code

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -4,20 +4,40 @@ extends Node3D
 
 @export var open: bool = false
 @export var locked = false
+var cardReaders = []
 
 func _ready():
 	if open == false:
 		animation.seek(0.0, true)
+	for child in self.get_children():
+		if get_tree().get_nodes_in_group("doorswitches").has(child):
+			cardReaders.append(child)
 
 func interact():
 	if !open and !locked:
-		open = true
-		animation.play("door_open")
+		openDoor()
 		print("Opening doors")
 	elif open:
-		open = false
-		animation.play_backwards("door_open")
+		closeDoor()
 		print("Closing doors")
 
-func unlockDoor(item):
-	pass
+func openDoor():
+	open = true
+	animation.play("door_open")
+
+func closeDoor():
+	open = false
+	animation.play_backwards("door_open")
+
+func _on_close_timer_timeout():
+	closeDoor()
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "door_open" and open == true:
+		$closeTimer.start()
+		for child in cardReaders:
+			child.openLight()
+	else:
+		$closeTimer.stop()
+		for child in cardReaders:
+			child.lockDoors()
--- Scripts/keycardReader.gd
+++ Scripts/keycardReader.gd
@@ -4,6 +4,7 @@ extends StaticBody3D
 @export var requiredColor: Color
 @onready var cardLight = $cardLight/indicatorLight
 @onready var unlockLight = $unlockLight/indicatorLight
+@onready var timer = $unlockLight/Timer
 signal press
 var door
 
@@ -13,14 +14,28 @@ func _ready():
 	if door.locked != true:
 		unlockLight.material_override.emission = Color(0, 255, 0, 255)
 			
+func lockDoors():
+	door.locked = true
+	unlockLight.material_override.emission = Color(255, 0, 0, 255)
 
 func unlockDoors():
 	door.locked = false
 	unlockLight.material_override.emission = Color(0, 255, 0, 255)
 
+func openLight():
+	unlockLight.material_override.emission = Color(255, 255, 255, 255)
+
 func readkeycard(card):
+	print("Reading card")
 	if card.color == requiredColor:
 		unlockDoors()
+	else:
+		unlockLight.material_override.emission_enabled = false
+		timer.start()
+		
+
+func _on_timer_timeout():
+	unlockLight.material_override.emission_enabled = true
 
 func _on_press():
 	door.interact()
