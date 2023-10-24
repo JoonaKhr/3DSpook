d3f40dd284a204a1aecb738a8bda1ecfa987fe76
Joona Kähärä <joonakhr@gmail.com>
2023-10-24 16:20:47 +0300

    ceiling light has animations and such

--- Scripts/light.gd
+++ Scripts/light.gd
@@ -1,10 +1,17 @@
 extends Node3D
 
+@export var isLightOn = false
 @onready var flickoff = $FlickoffTimer
 @onready var light = $OmniLight3D
 @onready var animations = $AnimationPlayer
+@onready var lighttube = $lightCeiling_tube/lightCeiling_tube
 
 func _ready():
+	if isLightOn:
+		light.visible = true
+	else:
+		light.visible = false
+		lighttube.material_override.emission_enabled = false
 	flickoff.wait_time = randf_range(120, 240)
 	
 
@@ -13,9 +20,11 @@ func interact():
 # While the light is on start the flickering animation
 	if light.visible:
 		flickoff.stop()
+		lighttube.material_override.emission_enabled = false
 		light.visible = false
 	else:
 		flickoff.start()
+		lighttube.material_override.emission_enabled = true
 		light.visible = true
 
 # Play spooky flickering animations
