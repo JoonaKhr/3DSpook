bcef2b746696072a8ef2c5d12e6623898f64b5bc
welitin14 <welitin.14@hotmail.fi>
2023-11-02 00:58:42 +0200

    small improvements to lightning effect

--- Scripts/PlayerandItems/item.gd
+++ Scripts/PlayerandItems/item.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var iname = ""
 @export var descrip = ""
 @export var color: Color
+var electricityMat = preload("res://Resources/Materials/m_gunElectricity.tres")
 signal press
 
 func _ready():
@@ -20,30 +21,27 @@ func shoot(target):
 	lineDraw(barrel, targetPoint)
 	#print("trying to draw I guess ?")
 	
-func lineDraw(from: Vector3, to: Vector3, meshcolor = Color.LIGHT_BLUE, persist_s = 0.1):
+func lineDraw(from: Vector3, to: Vector3, persist_s = 0.1):
 	var mesh_instance := MeshInstance3D.new()
 	var immediate_mesh := ImmediateMesh.new()
-	var material := load("res://Resources/Materials/m_gunElectricity.tres")
 	var directionVector = (to - from).normalized();
-	var playerDirection = get_parent().get_parent().get_parent().transform.basis.z
+	var playerDirection = get_parent().get_parent().get_parent().transform.basis.x * -1.0
 	var vertexDistance = 0.05
-	var stripThickness = 0.02
+	var stripThickness = 0.01
 	var amountOfVertexes = int(from.distance_to(to) / vertexDistance)
 	mesh_instance.mesh = immediate_mesh
 	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
-	immediate_mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP, material)
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP, electricityMat)
 	for i in amountOfVertexes:
-		var shift = (Vector3(0, 0, i % 2) * playerDirection).normalized()
+		var shift = (Vector3(playerDirection.x * (i % 2), 0, playerDirection.z * (i % 2))).normalized()
 		immediate_mesh.surface_add_vertex(from + directionVector * i * vertexDistance + shift * stripThickness)
 		immediate_mesh.surface_set_normal(Vector3(0, -1, 0))
 	for i in amountOfVertexes:
-		var shift = (Vector3(0, 0, i % 2) * playerDirection).normalized()
+		var shift = (Vector3(playerDirection.x * (i % 2), 0, playerDirection.z * (i % 2))).normalized()
 		immediate_mesh.surface_add_vertex(from + directionVector * (amountOfVertexes - i) * vertexDistance + shift * stripThickness)
 		immediate_mesh.surface_set_normal(Vector3(0, 1, 0))
 	immediate_mesh.surface_end()
 	#print(mesh_instance.mesh.surface_get_arrays(0))
-	#material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	#material.albedo_color = meshcolor
 	#print(get_parent().get_parent().get_parent().transform.basis)
 
 	get_tree().get_root().add_child(mesh_instance)
