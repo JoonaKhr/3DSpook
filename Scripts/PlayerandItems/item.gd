f27de142e0ebb0cdbe5bf44a9c2cf1898a603a9e
welitin14 <welitin.14@hotmail.fi>
2023-11-01 22:50:14 +0200

    lightningGun works

--- Scripts/PlayerandItems/item.gd
+++ Scripts/PlayerandItems/item.gd
@@ -18,21 +18,33 @@ func shoot(target):
 	var barrel = $Marker3D.global_position
 	var targetPoint = target["position"]
 	lineDraw(barrel, targetPoint)
-	print("trying to draw I guess ?")
+	#print("trying to draw I guess ?")
 	
-func lineDraw(from: Vector3, to: Vector3, meshcolor = Color.LIGHT_BLUE, persist_s = 0.5):
+func lineDraw(from: Vector3, to: Vector3, meshcolor = Color.LIGHT_BLUE, persist_s = 0.1):
 	var mesh_instance := MeshInstance3D.new()
 	var immediate_mesh := ImmediateMesh.new()
-	var material := ORMMaterial3D.new()
+	var material := load("res://Resources/Materials/m_gunElectricity.tres")
+	var directionVector = (to - from).normalized();
+	var playerDirection = get_parent().get_parent().get_parent().transform.basis.z
+	var vertexDistance = 0.05
+	var stripThickness = 0.02
+	var amountOfVertexes = int(from.distance_to(to) / vertexDistance)
 	mesh_instance.mesh = immediate_mesh
 	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
-	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
-	immediate_mesh.surface_add_vertex(from)
-	immediate_mesh.surface_add_vertex(to)
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP, material)
+	for i in amountOfVertexes:
+		var shift = (Vector3(0, 0, i % 2) * playerDirection).normalized()
+		immediate_mesh.surface_add_vertex(from + directionVector * i * vertexDistance + shift * stripThickness)
+		immediate_mesh.surface_set_normal(Vector3(0, -1, 0))
+	for i in amountOfVertexes:
+		var shift = (Vector3(0, 0, i % 2) * playerDirection).normalized()
+		immediate_mesh.surface_add_vertex(from + directionVector * (amountOfVertexes - i) * vertexDistance + shift * stripThickness)
+		immediate_mesh.surface_set_normal(Vector3(0, 1, 0))
 	immediate_mesh.surface_end()
-	print(mesh_instance.mesh.surface_get_arrays(0))
-	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	material.albedo_color = meshcolor
+	#print(mesh_instance.mesh.surface_get_arrays(0))
+	#material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	#material.albedo_color = meshcolor
+	#print(get_parent().get_parent().get_parent().transform.basis)
 
 	get_tree().get_root().add_child(mesh_instance)
 	if persist_s:
